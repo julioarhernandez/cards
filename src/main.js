cb5b59ef0b455398c820185fd8ce107cf4c326e1
Julio Rodriguez <juliorodriguez@ncl.com>
2023-01-19 15:37:47 -0500

    Added tippy tooltip to show card reward details

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './components/App/App.vue'
-import router from '@/router'
+import { createApp } from 'vue';
+import './style.css';
+import App from './components/App/App.vue';
+import router from '@/router';
+import VueTippy from 'vue-tippy';
+import 'tippy.js/dist/tippy.css';
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(router)
+    .use(VueTippy,{
+        defaultProps: {
+            maxWidth: 250
+        }
+    })
+    .mount('#app')
